1e53ec9127d0b22eed4a6aac6fe5fcae99e512f0
Dieb <clapisdave8@@gmail.com>
2026-09-22 15:00:37 +0800

    latest

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,44 +2,31 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\GymProfile;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 
 class HandleInertiaRequests extends Middleware
 {
-    /**
-     * The root template that's loaded on the first page visit.
-     *
-     * @see https://inertiajs.com/server-side-setup#root-template
-     *
-     * @var string
-     */
     protected $rootView = 'app';
 
-    /**
-     * Determines the current asset version.
-     *
-     * @see https://inertiajs.com/asset-versioning
-     */
     public function version(Request $request): ?string
     {
         return parent::version($request);
     }
 
-    /**
-     * Define the props that are shared by default.
-     *
-     * @see https://inertiajs.com/shared-data
-     *
-     * @return array<string, mixed>
-     */
     public function share(Request $request): array
     {
         $user = $request->user();
+        $gymProfile = GymProfile::current();
 
         return [
             ...parent::share($request),
             'name' => config('app.name'),
+            'gymProfile' => [
+                'name' => $gymProfile->name,
+                'cover_url' => $gymProfile->cover_url,
+            ],
             'auth' => [
                 'user' => $user,
                 'roles' => $user?->getRoleNames()->toArray() ?? [],
